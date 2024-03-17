0b519b048a3c85071cdaa8ebc3db50bf086af26c
Fabio <fabioescobarardila@hotmail.com>
2024-03-17 02:26:46 -0500

    Implementando Signals V1.1

--- src/app/signals/components/menu-bar/menu-bar.component.ts
+++ src/app/signals/components/menu-bar/menu-bar.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, WritableSignal, signal } from '@angular/core';
+
+interface MenuItem{
+  name: string,
+  route: string,
+}
 
 @Component({
-  selector: 'app-menu-bar',
+  selector: 'menu-bar',
   templateUrl: './menu-bar.component.html',
   styleUrl: './menu-bar.component.css'
 })
 export class MenuBarComponent {
+  // public menuItems: MenuItem[] = [
+  //   {name: 'Contador', route:'counter'},
+  //   {name: 'Usuario', route:'user-info'},
+  //   {name: 'Mutaciones', route:'properties'},
+  // ];
+
+  public menuItems: WritableSignal<MenuItem[]> = signal([
+    {name: 'Contador', route:'counter'},
+    {name: 'Usuario', route:'user-info'},
+    {name: 'Mutaciones', route:'properties'},
+  ])
 
 }
