c4169d47772ac4a3ef7fca7f9adfe880661d1ec8
Fabio <fabioescobarardila@hotmail.com>
2024-03-16 19:35:27 -0500

    Directiva terminada

--- src/app/shared/directives/label-custom.directive.ts
+++ src/app/shared/directives/label-custom.directive.ts
@@ -1,29 +1,50 @@
 import { Directive, ElementRef, Input, OnInit } from '@angular/core';
+import { ValidationErrors } from '@angular/forms';
 
 @Directive({
-  selector: '[label-custom]'
+  selector: '[label-custom]',
 })
-export class LabelCustomDirective implements OnInit{
+export class LabelCustomDirective implements OnInit {
   public _color: string = 'green';
+  public _errors?: ValidationErrors;
 
-  @Input()
-  set color(value: string) {
+  @Input() set color(value: string) {
     this._color = value;
     this.setStyle();
   }
 
-  constructor( public htmlElement: ElementRef<HTMLElement>) { console.log('Directive Constructor');
-   }
+  @Input() set errors(errors: ValidationErrors | undefined | null) {
+    if (!errors) {
+      this.htmlElement.nativeElement.innerHTML = '';
+      return;
+    }
+    this._errors = errors;
+    this.printErrors();
+  }
+
+  constructor(public htmlElement: ElementRef<HTMLElement>) {}
 
   ngOnInit(): void {
-    this.htmlElement.nativeElement.innerHTML = 'El error es fatal'
+    this.setStyle();
   }
 
   setStyle(): void {
-    if( !this.htmlElement ) return;
+    if (!this.htmlElement) return;
 
     this.htmlElement!.nativeElement.style.color = this._color;
   }
 
+  printErrors() {
+    let errorKeys: string[] = Object.keys(this._errors!);
 
+    if (errorKeys.includes('required')){
+      this.htmlElement.nativeElement.innerHTML = 'El campo es requerido <br>';
+    }
+    if (errorKeys.includes('email')) {
+      this.htmlElement.nativeElement.innerHTML = 'El email no es valido';
+    };
+    if (errorKeys.includes('minlength')) {
+      this.htmlElement.nativeElement.innerHTML = 'El nombre debe tener 6 caracteres minimo';
+    };
+  }
 }
